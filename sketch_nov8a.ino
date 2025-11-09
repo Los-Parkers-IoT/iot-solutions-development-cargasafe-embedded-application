4f309c16c919f2c6d1eadda0f31406d6f3ee2525
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2025-11-09 01:24:40 -0500

    feat(embeddedApp): updated sketch_nov8a.ino

--- sketch_nov8a.ino
+++ sketch_nov8a.ino
@@ -1,11 +1,18 @@
 #include "ModestIoT.h"
+#include "EdgeCommunicatorHttp.h"
 
 #define RXD2 16
 #define TXD2 17
 #define DHTPIN 25
 
+const char* WIFI_SSID = "IntroduzcaWIFI";
+const char* WIFI_PASS = "IntroduzcaContraseña";
+
+const char* EDGE_URL = "https://iot-solutions-development-cargasafe-edge.onrender.com/api/v1/telemetry-monitoring/data-records";
+
 HardwareSerial neogps(1);
-DataController controller;
+EdgeCommunicatorHttp edge(WIFI_SSID, WIFI_PASS, EDGE_URL);
+DataController controller(&edge);
 
 GPSSensor gps(&neogps, RXD2, TXD2, &controller);
 DHTSensor dht(DHTPIN, &controller);
@@ -15,7 +22,13 @@ unsigned long lastReadGPS = 0;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("=== Modest IoT Framework ===");
+  Serial.println("=== Modest IoT Framework (HTTP MODE) ===");
+
+  edge.begin();
+
+  configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+  Serial.println("Sincronizando hora NTP...");
+  delay(1000); 
 
   gps.begin();
   dht.begin();
@@ -26,12 +39,12 @@ void setup() {
 void loop() {
   unsigned long now = millis();
 
-  if (now - lastReadGPS >= 1000) {
+  if (now - lastReadGPS >= 2000) {
     gps.read();
     lastReadGPS = now;
   }
 
-  if (now - lastReadDHT >= 2000) {
+  if (now - lastReadDHT >= 3000) {
     dht.read();
     lastReadDHT = now;
   }
