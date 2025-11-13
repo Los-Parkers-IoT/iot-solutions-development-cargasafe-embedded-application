62a5573bab86f8440ec8817d51ee9a450e3d7543
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2025-11-13 15:01:00 -0500

    refactor(embeddedApp): update DHTSensor.cpp

--- DHTSensor.cpp
+++ DHTSensor.cpp
@@ -1,7 +1,7 @@
 #include "DHTSensor.h"
 
 DHTSensor::DHTSensor(int pin, EventHandler* handler)
-  : Sensor(pin, handler), dht(pin, DHT11) {}
+  : Sensor(pin, handler), dht(pin, DHT11), lastTemperature(0.0), lastHumidity(0.0) {}
 
 void DHTSensor::begin() {
   dht.begin();
@@ -15,6 +15,9 @@ void DHTSensor::read() {
   if (isnan(h) || isnan(t)) {
     on(Event(EVENT_ERROR, "Error reading DHT11"));
   } else {
+    lastHumidity = h;
+    lastTemperature = t;
+    
     String data = "Temp: " + String(t) + " °C | Hum: " + String(h) + " %";
     on(Event(EVENT_DHT_UPDATE, data));
   }
